1c4e1c14e89be15aa3aa2a1f9d7397cc82233b2d
Lia Papailiou <evangelia.papailiou@gmail.com>
2020-08-22 16:01:29 +0200

    clear code

--- src/components/ArtistList.js
+++ src/components/ArtistList.js
@@ -1,24 +1,18 @@
 import React, { useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import shortid from 'shortid';
+
 const ArtistList = (props) => {
-  const [sortedField, setSortedField] = useState(null);
+  const [sortedArtist, setSortedArtist] = useState(null);
   const { artist } = props;
-  const sorted = [...artist]
+  const sorted = [...artist];
+
   useMemo(() => {
-    sorted.map((item) => {
-      if (item.id === 0) {
-        sorted.shift(item);
-      }
-    })
-    if (sortedField !== null) {
-      sorted.sort((a, b) => a.artistStars > b.artistStars ? -1 : 1);
-    };
-    setSortedField(sorted);
+    sorted.sort((a, b) => a.artistStars > b.artistStars ? -1 : 1);
+    setSortedArtist(sorted);
     return sorted;
   }, [artist]);
 
-  console.log('SORTED:', sortedField);
   return (
     <div>
       <table>
@@ -30,10 +24,10 @@ const ArtistList = (props) => {
           </tr>
         </thead>
         <tbody>
-          {Array.isArray(sortedField) && sortedField.map((item) => {
+          {Array.isArray(sortedArtist) && sortedArtist.map((item) => {
             if (item.id > 0) {
               return (
-                <tr key={shortid.generate()} style={{ listStyleType: 'none' }}>
+                <tr key={shortid.generate()}>
                   <td style={{ paddingRight: '2em', paddingLeft: '2em', paddingBottom: '1em' }}>
                     <Link to={`/artist/${item.id}`}>
                       {item.artistName}
