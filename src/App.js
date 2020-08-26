4f1699f62dbfe1430cd7c0c1cd3002e8b9e496d6
Lia Papailiou <evangelia.papailiou@gmail.com>
2020-08-26 11:45:55 +0200

    split reducer from app

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import ArtistContext from './ArtistContext';
 import AddArtist from './components/AddArtist';
 import EditArtist from './components/EditArtist';
 import ArtistList from './components/ArtistList';
+import artistReducer from './reducer/artistReducer';
 import {
   ADD_ARTIST,
   EDIT_ARTIST,
@@ -27,65 +28,6 @@ const INITIAL_STATE = {
       artistStars: 0,
     },
   ],
-};
-
-const artistReducer = (state, action) => {
-  const { type } = action;
-  const newCounter = state.counter + 1;
-  const newArtist = {
-    id: newCounter,
-    artistName: action.artistName,
-    artistPhoto: '',
-    artistStars: 0,
-  };
-  const idxInc = state.artist.findIndex((item) => item.id === action.id);
-  const tempInc = [...state.artist];
-  const idxDec = state.artist.findIndex((item) => item.id === action.id);
-  const tempDec = [...state.artist];
-  const artistNew = {
-    id: action.id,
-    artistName: action.artistName,
-    artistPhoto: action.artistPhoto,
-    artistStars: action.artistStars,
-  };
-  const idx = state.artist.findIndex((item) => item.id === action.id);
-  switch (type) {
-    case ADD_ARTIST:
-      return {
-        counter: newCounter,
-        artist: [...state.artist, newArtist],
-      };
-    case EDIT_ARTIST:
-      return {
-        ...state,
-        artist: [...state.artist.slice(0, idx), artistNew, ...state.artist.slice(idx + 1)],
-      };
-    case CHANGE_RATING_INC:
-      return {
-        ...state,
-        artist: [
-          ...state.artist.slice(0, idxInc),
-          { ...tempInc[idxInc], artistStars: tempInc[idxInc].artistStars + 1 },
-          ...state.artist.slice(idxInc + 1),
-        ],
-      };
-    case CHANGE_RATING_DEC:
-      if (tempDec[idxDec].artistStars < 1) {
-        return {
-          ...state,
-        };
-      }
-      return {
-        ...state,
-        artist: [
-          ...state.artist.slice(0, idxDec),
-          { ...tempDec[idxDec], artistStars: tempDec[idxDec].artistStars - 1 },
-          ...state.artist.slice(idxDec + 1),
-        ],
-      };
-    default:
-      return INITIAL_STATE;
-  }
 };
 
 function App() {
@@ -133,7 +75,7 @@ function App() {
           </Switch>
         </Router>
       </ArtistContext.Provider>
-      {/* <pre>{JSON.stringify(state, null, 2)}</pre> */}
+      {/* <pre>{JSON.stringify(state, null, 2)}</pre> */ }
     </div>
   );
 }
---
+++ src/reducer/artistReducer.js
@@ -0,0 +1,79 @@
+import {
+  ADD_ARTIST,
+  EDIT_ARTIST,
+  CHANGE_RATING_INC,
+  CHANGE_RATING_DEC,
+} from '../types';
+
+const INITIAL_STATE = {
+  counter: 0,
+  artist: [
+    {
+      id: 0,
+      artistName: '',
+      artistPhoto: '',
+      artistStars: 0,
+    },
+  ],
+};
+
+const artistReducer = (state, action) => {
+  const { type } = action;
+  const newCounter = state.counter + 1;
+  const newArtist = {
+    id: newCounter,
+    artistName: action.artistName,
+    artistPhoto: '',
+    artistStars: 0,
+  };
+  const idxInc = state.artist.findIndex((item) => item.id === action.id);
+  const tempInc = [...state.artist];
+  const idxDec = state.artist.findIndex((item) => item.id === action.id);
+  const tempDec = [...state.artist];
+  const artistNew = {
+    id: action.id,
+    artistName: action.artistName,
+    artistPhoto: action.artistPhoto,
+    artistStars: action.artistStars,
+  };
+  const idx = state.artist.findIndex((item) => item.id === action.id);
+  switch (type) {
+    case ADD_ARTIST:
+      return {
+        counter: newCounter,
+        artist: [...state.artist, newArtist],
+      };
+    case EDIT_ARTIST:
+      return {
+        ...state,
+        artist: [...state.artist.slice(0, idx), artistNew, ...state.artist.slice(idx + 1)],
+      };
+    case CHANGE_RATING_INC:
+      return {
+        ...state,
+        artist: [
+          ...state.artist.slice(0, idxInc),
+          { ...tempInc[idxInc], artistStars: tempInc[idxInc].artistStars + 1 },
+          ...state.artist.slice(idxInc + 1),
+        ],
+      };
+    case CHANGE_RATING_DEC:
+      if (tempDec[idxDec].artistStars < 1) {
+        return {
+          ...state,
+        };
+      }
+      return {
+        ...state,
+        artist: [
+          ...state.artist.slice(0, idxDec),
+          { ...tempDec[idxDec], artistStars: tempDec[idxDec].artistStars - 1 },
+          ...state.artist.slice(idxDec + 1),
+        ],
+      };
+    default:
+      return INITIAL_STATE;
+  }
+};
+
+export default artistReducer;
